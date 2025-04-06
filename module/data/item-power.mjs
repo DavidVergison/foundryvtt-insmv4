8e79a068d3bebdbadfefb0008a5711ca9c654d75
DavidVergison <davidvergison@gmail.com>
2025-04-06 09:20:31 +0200

    divers

--- module/data/item-power.mjs
+++ module/data/item-power.mjs
@@ -7,6 +7,10 @@ export default class InsMvPower extends InsMvItemBase {
     const schema = super.defineSchema();
 
     schema.level = new fields.StringField({ required: true, nullable: false });
+    schema.type = new fields.StringField({ required: false, nullable: true });
+    schema.activation = new fields.StringField({ required: false, nullable: true });
+    schema.cost = new fields.StringField({ required: false, nullable: true });
+    schema.quality = new fields.StringField({ required: false, nullable: true });
 
     return schema;
   }
